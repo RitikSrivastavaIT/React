006e1a82547adb85c92ebd9c1daae3adcde4edf5
Ritik Srivastava <ritiksrivastava.09.com>
2024-09-23 22:44:07 +0530

    Added a Key to a component.

--- Projects/todo-app/src/components/TodoItems.jsx
+++ Projects/todo-app/src/components/TodoItems.jsx
@@ -1,11 +1,14 @@
 import TodoItem from "./TodoItem";
 import styles from "./TodoItems.module.css";
-// className={styles.itemContainer}
 const TodoItems = ({ todoItems }) => {
   return (
     <div className={styles.itemContainer}>
       {todoItems.map((items) => (
-        <TodoItem todoName={items.name} todoDate={items.date} />
+        <TodoItem
+          key={items.name}
+          todoName={items.name}
+          todoDate={items.date}
+        />
       ))}
     </div>
   );
